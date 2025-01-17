2645fd1842361280c2c0d0cdbf18693f8fbc3b9c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-01-17 01:03:02 +0100

    Fix twitch & discord oauth url

--- src/rocket/discord/oauth/new.rs
+++ src/rocket/discord/oauth/new.rs
@@ -6,6 +6,6 @@ const SCOPES: &'static str = "identify connections guilds guilds.members.read";
 #[rocket::get("/discord/new_oauth")]
 pub async fn new_oauth<'r>(auth: &rocket::State<Arc<crate::rocket::auth::Auth>>) -> rocket::response::Redirect {
     let token = auth.get_new_csrf().await;
-    let uri = BASE_URI.append("/".into(), Some(format!("response_type=code&prompt=none&scope={SCOPES}redirect_uri={}&client_id={}&state={token}", crate::rocket::auth::discord::OAUTH_URL, auth.twitch.client_id).into()));
+    let uri = BASE_URI.append("/".into(), Some(format!("response_type=code&prompt=none&scope={SCOPES}&redirect_uri={}&client_id={}&state={token}", crate::rocket::auth::discord::OAUTH_URL, auth.twitch.client_id).into()));
     rocket::response::Redirect::temporary(uri)
 }
--- src/rocket/twitch/oauth/new.rs
+++ src/rocket/twitch/oauth/new.rs
@@ -5,6 +5,6 @@ const BASE_URI: rocket::http::uri::Absolute<'static> = rocket::uri!("https://id.
 #[rocket::get("/twitch/new_oauth")]
 pub async fn new_oauth<'r>(auth: &rocket::State<Arc<crate::rocket::auth::Auth>>) -> rocket::response::Redirect {
     let token = auth.get_new_csrf().await;
-    let uri = BASE_URI.clone().append("/".into(), Some(format!("response_type=code&scope=redirect_uri={}&client_id={}&state={token}", crate::rocket::auth::twitch::OAUTH_URL, auth.twitch.client_id).into()));
+    let uri = BASE_URI.clone().append("/".into(), Some(format!("response_type=code&scope=&redirect_uri={}&client_id={}&state={token}", crate::rocket::auth::twitch::OAUTH_URL, auth.twitch.client_id).into()));
     rocket::response::Redirect::temporary(uri)
 }
