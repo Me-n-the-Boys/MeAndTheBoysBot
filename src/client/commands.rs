d4ae1b3f3dede51db4e8b3583a426fde5b23e622
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-01-29 20:41:21 +0100

    Implement emoji copying

--- src/client/commands.rs
+++ src/client/commands.rs
@@ -26,3 +26,58 @@ pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 
 pub use debug::debug;
 pub use settings::settings;
+
+
+///Copies one emoji to the guild the command was run in (5 min cooldown).
+#[poise::command(
+    slash_command,
+    guild_only,
+    user_cooldown = 300,
+    default_member_permissions = "MANAGE_GUILD_EXPRESSIONS",
+    required_bot_permissions = "MANAGE_GUILD_EXPRESSIONS",
+)]
+pub async fn copy_emoji(ctx: Context<'_>, #[description = "Exactly ONE Discord emoji"] #[min_length = 1] emoji: String, #[description = "Name of the emoji in the guild"] #[min_length = 2] name: String) -> Result<(), Error> {
+    let emoji_p = match serenity::utils::parse_emoji(emoji) {
+        Some(v) => v,
+        None => {
+            ctx.send(CreateReply::default().content("Expected the emoji argument to contain exactly one discord emoji. Please remove any other content, such as extra spaces in-front or behind the emoji.").ephemeral(true).reply(true)).await?;
+            return Ok(());
+        }
+    };
+
+    if emoji_p.name.chars().any(|c| !c.is_alphanumeric() && c != '_') {
+        ctx.send(CreateReply::default().content("The name of the emoji must be at least 2 characters long and can only contain alphanumeric characters and underscores.").ephemeral(true).reply(true)).await?;
+        return Ok(());
+    }
+
+    let guild = match ctx.guild().map_or(Err("This command must be run in a guild."), |guild|{
+        if guild.emojis.get(&emoji_p.id).is_some() {
+            return Err("This emoji comes from the guild, in which this command was ran.");
+        }
+
+        if guild.emojis.values().filter(|v|v.name == name).any(|_|true) {
+            return Err("This emoji comes from the guild, in which this command was ran.");
+        }
+
+        Ok(guild.id)
+    }) {
+        Err(err) => {
+            ctx.send(CreateReply::default().content(err).ephemeral(true).reply(true)).await?;
+            return Ok(());
+        }
+        Ok(v) => v
+    };
+
+    let image = reqwest::Client::new().get(emoji_p.url()).send().await?.bytes().await?;
+    let image = base64::Engine::encode(&base64::engine::general_purpose::STANDARD, image.as_ref());
+    let emoji = guild.create_emoji(&ctx, &name, &image).await?;
+    ctx.send(CreateReply::default().content(
+        format!(
+            "Copied emoji: <{}:{}:{}>",
+            if emoji.animated {"a"} else {""},
+            emoji.name,
+            emoji.id.get()
+        )
+    ).reply(true)).await?;
+    Ok(())
+}
