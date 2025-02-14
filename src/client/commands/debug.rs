43c52b609e31d20d1a682dc00af012aa84f7d4c1
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-02-14 22:08:28 +0100

    Fix compile

--- src/client/commands/debug.rs
+++ src/client/commands/debug.rs
@@ -10,7 +10,7 @@ pub async fn debug(ctx: Context<'_>) -> Result<(), Error> {
     let handler = &ctx.data().handler;
     let mut reply = ctx.reply_builder(poise::CreateReply::default());
     let content = serde_json::to_vec(handler)?;
-    reply = reply.attachment(poise::serenity::CreateAttachment::bytes(content, "state.json"));
+    reply = reply.attachment(serenity::builder::CreateAttachment::bytes(content, "state.json"));
     ctx.send(reply).await?;
     Ok(())
 }
