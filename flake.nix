ffd1df8287898f533e642c290cfd41ca91442fde
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-02-25 20:41:47 +0100

    Fix Workdir not existing

--- flake.nix
+++ flake.nix
@@ -87,6 +87,10 @@
                 Cmd = [ "${nixpkgs.lib.getExe package}" ];
             };
 
+            extraCommands = ''
+                mkdir -p /data
+            '';
+
             created = "${builtins.substring 0 4 self.lastModifiedDate}-${builtins.substring 4 2 self.lastModifiedDate}-${builtins.substring 6 2 self.lastModifiedDate}T${builtins.substring 8 2 self.lastModifiedDate}:${builtins.substring 10 2 self.lastModifiedDate}:${builtins.substring 12 2 self.lastModifiedDate}Z";
         };
       in
